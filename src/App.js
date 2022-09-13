93378497f99d928a9aa755d6ee6f4903120584da
Raga <ragawilliam570@gmail.com>
2022-09-13 14:29:09 +0300

    Add Header to main calculator page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import './css/App.css';
 
 import CalculatorComponent from './components/Calculator';
+import Header from './components/Header';
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class App extends React.Component {
   render() {
     return (
-      <div className="container">
-        <CalculatorComponent />
-      </div>
+      <>
+        <Header />
+        <div className="container">
+          <div className="welcomeContainer">
+            <h1>Let&apos;s do some Math</h1>
+          </div>
+          <CalculatorComponent />
+        </div>
+      </>
     );
   }
 }
