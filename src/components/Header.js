90829b92fcfc1a8dd877b6c7afb23c7e4f35c89c
Raga <ragawilliam570@gmail.com>
2022-09-13 14:28:22 +0300

    Add link to Header, corrected path names and connected to respective pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
+import { Link } from 'react-router-dom';
+
 import style from './Header.module.css';
 
 const Header = () => (
   <header className={style.headerContainer}>
     <a href="###" className={style.logo}>Math Magicians</a>
     <ul className={style.navLinksContainer}>
-      <li className={style.navLink}><a href="###">Home</a></li>
-      <li className={style.navLink}><a href="###">Calculator</a></li>
-      <li className={style.navLink}><a href="###">Quote</a></li>
+      <li className={style.navLink}><Link to="/">Home</Link></li>
+      <li className={style.navLink}><Link to="/calculator">Calculator</Link></li>
+      <li className={style.navLink}><Link to="/quote">Quote</Link></li>
     </ul>
   </header>
 );
